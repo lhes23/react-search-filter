b8e7c41d4b6d3f587904108d8b26b0f4597dea67
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-02-07 21:38:01 +0800

    added search filter function and remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import jsonData from "./MOCK_DATA.json";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const data = jsonData
+    .filter((val) => {
+      if (
+        val.first_name.toLowerCase().includes(search.toLowerCase()) ||
+        val.last_name.toLowerCase().includes(search.toLowerCase()) ||
+        val.email.toLowerCase().includes(search.toLowerCase())
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .map((val, key) => {
+      return (
+        <tr key={key}>
+          <td>{val.id}</td>
+          <td>{val.first_name}</td>
+          <td>{val.last_name}</td>
+          <td>{val.email}</td>
+        </tr>
+      );
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Enter search"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>{data}</tbody>
+      </table>
     </div>
   );
 }
